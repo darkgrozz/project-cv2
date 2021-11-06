c37685bf1ac7f281b6942202771759736452dff2
darkgrozz <darkgroz046@gmail.com>
2021-11-06 09:25:53 +0700

    project-cv2

---
+++ src/components/CvSatu.vue
@@ -0,0 +1,156 @@
+<template>
+  <div id="c">
+    <div v-for="cv in listCv" :key="cv">
+      <div id="a">
+        <div id="d">
+          <h3 id="g" style="margin-top: 50px">
+            {{ cv.nama }} <br />
+            <font id="e"> {{ cv.nama1 }}</font>
+          </h3>
+          <img src="gambar.jpg" width="315" height="165" />
+          <h3 id="f" v-for="cv in listCv" :key="cv">
+            {{ cv.contact }}
+          </h3>
+          <p id="f">
+            <b>{{ cv.judul1 }}</b
+            ><br />{{ cv.address }}
+          </p>
+          <p id="f">
+            <b>{{ cv.judul2 }}</b
+            ><br />{{ cv.phone }}
+          </p>
+          <p id="f">
+            <b>{{ cv.judul3 }}</b
+            ><br /><u>{{ cv.email }}</u>
+          </p>
+          <p id="f">
+            <b>{{ cv.judul4 }}</b
+            ><br /><u>{{ cv.link }}</u>
+          </p>
+          <h3 id="f">
+            {{ cv.judul5 }}
+            <hr />
+          </h3>
+          <p id="f">
+            {{ cv.bahasa1 }}<br />{{ cv.bahasa2 }}<br />{{ cv.bahasa3 }}
+          </p>
+          <h3 id="f">
+            {{ cv.judul6 }}
+            <hr />
+          </h3>
+          <ul id="f">
+            <li>{{ cv.hobi1 }}</li>
+            <li>{{ cv.hobi2 }}</li>
+            <li>{{ cv.hobi3 }}</li>
+            <li>{{ cv.hobi4 }}</li>
+          </ul>
+        </div>
+        <div id="b">
+          <h2 style="margin-top: 50px">Summary</h2>
+          <hr />
+          <p>
+            {{ cv.summary }}
+          </p>
+          <h2>Skill HighLights</h2>
+          <hr />
+          <div style="display: flex">
+            <div>
+              <ul>
+                <li>{{ cv.skil1 }}</li>
+                <li>{{ cv.skil2 }}</li>
+                <li>{{ cv.skil3 }}</li>
+              </ul>
+            </div>
+            <ul>
+              <li>{{ cv.skil4 }}</li>
+              <li>{{ cv.skil5 }}</li>
+              <li>{{ cv.skil6 }}</li>
+            </ul>
+          </div>
+          <h2>Experience</h2>
+          <hr />
+          <p>
+            <b>{{ cv.ex1 }}</b
+            >{{ cv.ex2 }}<br /><b>{{ cv.ex3 }}</b>
+            {{ cv.ex4 }}
+          </p>
+          <ul>
+            <li>
+              {{ cv.des1 }}
+            </li>
+            <li>{{ cv.des2 }}</li>
+            <li>
+              {{ cv.des3 }}
+            </li>
+            <li>
+              {{ cv.des4 }}
+            </li>
+          </ul>
+          <h2>Education</h2>
+          <hr />
+          <p>
+            {{ cv.edu1 }}<b>{{ cv.edu2 }}</b
+            >{{ cv.edu3 }}<br />
+            <b>{{ cv.edu4 }}</b> NY
+          </p>
+
+          <h2>Certifications</h2>
+          <hr />
+          <p>
+            {{ cv.creative }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CvDua",
+  computed: {
+    listCv() {
+      return this.$store.state.listCv;
+    },
+  },
+};
+</script>
+
+<style>
+#a {
+  display: flex;
+  background-color: rgb(189, 189, 189);
+}
+#b {
+  background-color: white;
+  padding-left: 10px;
+  padding-right: 20px;
+  padding-bottom: 100px;
+}
+#c {
+  margin-right: 300px;
+  padding-left: 300px;
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+#d {
+  font-family: sans-serif;
+  padding: auto;
+}
+#e {
+  color: rgb(255, 102, 0);
+}
+#f {
+  margin-right: 20px;
+  margin-left: 20px;
+}
+#g {
+  margin-right: 20px;
+  margin-left: 20px;
+  font-size: 35px;
+}
+body {
+  background-color: gray;
+  font-family: sans-serif;
+}
+</style>
---
+++ src/store.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        listCv: [
+            { nama: 'Rizky',
+            nama1: 'Putra',
+            contact: 'Contact',
+            address: 'Semarang ngaliyan podorejo grujugan rt/01 rw/07',
+            phone: '088215019380',
+            email: 'darkgroz046@gmail.com',
+            link: 'linkedln.com/darkgroz',
+            summary: 'Senior Web Developer specializing in front end development. Experience with all stages of the development cycle for dynamic web projects. well-versed in numerous programming. lenguages including HTML5, PHP OPP, JavaScript, CSS, MySQL. Strong background in project management and customer relations.',
+            bahasa1: 'Spanish-C2',
+            bahasa2: 'Chinese-A1',
+            bahasa3: 'German-A2',
+            hobi1: 'Writing',
+            hobi2: 'Sketcing',
+            hobi3: 'Photograpy',
+            hobi4: 'Design',
+            skil1: 'Project Management',
+            skil2: 'Strong decision maker',
+            skil3: 'Complex Problem Solver',
+            skil4: 'Creative Design',
+            skil5: 'Inovative',
+            skil6: 'Service-focused',
+            ex1: 'Web Developer',
+            ex2: '-09/2015 to 05/2019',
+            ex3: 'Luna Web Design,',
+            ex4: 'New York',
+            des1: 'Cooprate with designers to create clean interfaces and simple, intuitive interactions and experiences',
+            des2: 'Develop project concepts and maintain optimal workflow',
+            des3: 'Work with senior developer to manage large, complex design project for corporate clients.',
+            des4: 'Complete detaild programming and development tasks for front end public and internal website as well as',
+            edu1: 'Bachelor of Science: ',
+            edu2: 'Computer Information System',
+            edu3: ' - 2014',
+            edu4: 'Columbia University,',
+            creative: 'PHP Framework (certificate): Programming Zen, Codeigniter,Symfony. Programming Languages: JavaScript,HTML5, PHP OOP, CSS, SQL,MySQL',
+            judul1: 'address :',
+            judul2: 'Phone :',
+            judul3: 'Email :',
+            judul4: 'Linkedin :',
+            judul5: 'Bahasa',
+            judul6: 'Hobbies'
+        },
+    ]
+},
+mutations: {
+
+},
+actions: {
+
+}
+})
